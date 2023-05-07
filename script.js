39345a2a32943e1e273d37cbde559ab1f1f23500
DANIYEL ARUSHANOV <128869869+darushanov02@users.noreply.github.com>
2023-05-06 22:38:28 -0700

    js script

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function generateTable(){
+
+var getData = document.getElementById("number").value;
+var putData = document.getElementById("blackboard");
+var i, temp;
+for (i=1; i<10; i++) {
+    putData.innerHTML+= getData + "X" + i + "=" + getData * 1 + "<br>";
+}
+}
+
